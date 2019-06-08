75a316f2fc3c6fb954729da8a38e8ec2641a6fa3
Dmitriy Kondin <kondin@etown.ru>
2019-06-08 16:04:09 +0700

    - добавил функцию native_insert

--- api_example.php
+++ api_example.php
@@ -1,8 +1,16 @@
 <?php
 echo '<pre>';
 $result = get_auth('admin', 'admin');
+$session_key = $result['session_key'];
 print_r($result);
-$result = load_grid_columns('data', $result['session_key']);
+$ql_items = array(
+    'topic_id' => 1,
+    'city_id' => 1,
+    'text' => 'Объявление из API',
+    'price' => '5000000');
+$result = native_insert('data', $ql_items,$session_key);
+print_r($result);
+$result = load_grid_columns('data', $session_key);
 print_r($result);
 echo '</pre>';
 
@@ -13,6 +21,19 @@ function load_grid_columns ($model_name, $session_key) {
         'do' => 'load_grid_columns',
         'session_key' => $session_key,
         'model_name' => $model_name,
+    );
+    $result = executeHTTPRequest($queryUrl, $params);
+    return $result;
+}
+
+function native_insert ( $model_name, $ql_items, $session_key ) {
+    $queryUrl = 'http://estate.sitebill.ru/apps/api/rest.php';
+    $params = array(
+        'action' => 'model',
+        'do' => 'native_insert',
+        'session_key' => $session_key,
+        'model_name' => $model_name,
+        'ql_items' => $ql_items,
     );
     $result = executeHTTPRequest($queryUrl, $params);
     return $result;
